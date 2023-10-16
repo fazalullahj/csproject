a8c5b7614089b706bded198d8475c0aec210a3bf
Fazalullah Jafar <fazalullahjafar@gmail.com>
2023-10-16 11:49:49 +0400

    working on product viewing and adding

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import mysql.connector as mysql
-#signin and sign up
+print("\t\t ABC Supermarket, Dubai branch\t\t")
+#signin and sign up function -->
 def authentication():
     choice = int(input('Sign In : 1\nSign Up : 2\nEnter your choice number: '))
 #sign in
@@ -44,8 +45,38 @@ while True:
             break
     except:
         print("Failed to connect to MySQL ")
-cursor = con.cursor()
+#main menu
+def menu():
+    while True:
+        choice = int(input("View products : 1\nAdd products : 2\nMake sale : 3\nQuit : 4\n Enter choice number: "))
+        if choice == 1:
+            viewproduct()
+        elif choice == 2:
+            addproduct()
+        elif choice == 3:
+            sale()
+        elif choice == 4:
+            break
 
+def viewproduct():
+    cursor.execute("Select * from product;")
+    allproducts = cursor.fetchall()
+    column_names = [desc[0] for desc in cursor.description]
+    #if no products
+    if len(allproducts) == 0:
+        print('No products')
+    else:#displaying the table of data in a nice format
+        print("\t".join(column_names))  # Print columnnames
+        for row in allproducts:
+            formatted_row = "\t".join(map(str, row))
+            print(formatted_row)
+
+
+def addproduct():
+        pass
+
+        
+cursor = con.cursor()
 cursor.execute("CREATE DATABASE IF NOT EXISTS my_database;")
 cursor.execute('USE my_database;')
 
@@ -58,6 +89,8 @@ cursor.execute('create table if not exists product(pID int(20) primary key, pnam
 cursor.execute('create table if not exists sales(sID int(20) primary key, pID int, quantity_sold int(25) , total_price float(10,2), sale_date date, cashier varchar(25), foreign key (pID) references product(pID), foreign key (cashier) references user_details(uname));')
 
 
+
 currentUser = ''
 currentRole = ''
 authentication()
+menu()
