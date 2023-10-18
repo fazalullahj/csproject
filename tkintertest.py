8f6e5b3ee7ffe08909c46477e9536b73e5ed6489
Fazalullah Jafar <fazalullahjafar@gmail.com>
2023-10-18 23:06:43 +0400

    Update tkintertest.py

--- tkintertest.py
+++ tkintertest.py
@@ -2,44 +2,56 @@ from tkinter import *
 import mysql.connector as mysql
 from tkinter import messagebox
 from tkinter import ttk
+from ttkthemes import ThemedTk
 
-root = Tk()
+root = ThemedTk()
 root.title("Raz's Supermarket, Dubai Br.")
-root.geometry("600x650")
+root.geometry("300x350")
+
+style = ttk.Style(root)
+style.theme_use("breeze")  # 'winnative', 'clam', 'alt', 'default' and more...
+
+
 def auth():
-    global con,cursor
+    global con, cursor
     try:
         password = auth_entry.get()
         con = mysql.connect(host="localhost", user="root", password=password)
         cursor = con.cursor()
         cursor.execute("CREATE DATABASE IF NOT EXISTS my_database;")
-        cursor.execute('USE my_database;')
+        cursor.execute("USE my_database;")
 
         # all tables creation-->
         # user details
-        cursor.execute('create table if not exists user_details( uname varchar(25) primary key, password varchar(25) , urole varchar(15));')
+        cursor.execute(
+            "create table if not exists user_details( uname varchar(25) primary key, password varchar(25), urole varchar(15));"
+        )
         # product
-        cursor.execute('create table if not exists product(pID int(20) primary key, pname varchar(25), price float(10,2) , stock int(15));')
+        cursor.execute(
+            "create table if not exists product(pID int(20) primary key, pname varchar(25), price float(10,2), stock int(15));"
+        )
         # sales
-        cursor.execute('create table if not exists sales(sID int(20) primary key, pID int, quantity_sold int(25) , total_price float(10,2), sale_date date, cashier varchar(25), foreign key (pID) references product(pID), foreign key (cashier) references user_details(uname));')        
+        cursor.execute(
+            "create table if not exists sales(sID int(20) primary key, pID int, quantity_sold int(25), total_price float(10,2), sale_date date, cashier varchar(25), foreign key (pID) references product(pID), foreign key (cashier) references user_details(uname));"
+        )
         if con.is_connected():
             auth_frame.destroy()
             sign_frame.pack()
-            return con,cursor
+            return con, cursor
     except Exception as e:
-        messagebox.showerror("Error","Wrong Password for MySQL.")
+        messagebox.showerror("Error", "Wrong Password for MySQL.")
 
 
 def sign_in():
     sign_frame.destroy()
     sign_in_frame.pack()
-    uname_label = Label(sign_in_frame,text = "Username")
-    password_label = Label(sign_in_frame,text = "Password")
+    uname_label = ttk.Label(sign_in_frame, text="Username")
+    password_label = ttk.Label(sign_in_frame, text="Password")
     uname_label.pack()
-    uname_entry = Entry(sign_in_frame, text="Username")
+    uname_entry = ttk.Entry(sign_in_frame, text="Username")
     uname_entry.pack()
     password_label.pack()
-    password_entry = Entry(sign_in_frame, text="Password", show="*")
+    password_entry = ttk.Entry(sign_in_frame, text="Password", show="*")
     password_entry.pack()
 
     def submit():
@@ -51,55 +63,62 @@ def sign_in():
             if row is not None and password == row[1]:
                 global currentUser, currentRole
                 currentUser, currentRole = uname, row[2]
-                sign_in_label = Label(menu_frame, text=f"Signed in as {currentUser} - {currentRole}")
+                sign_in_label = ttk.Label(
+                    menu_frame, text=f"Signed in as {currentUser} - {currentRole}"
+                )
                 sign_in_label.pack()
                 menu_frame.pack()
                 sign_in_frame.destroy()
         except Exception as e:
             print(e)
-            print('Error!')
+            print("Error!")
             quit()
 
-    sign_in_submit = Button(sign_in_frame, text="Submit", command=submit)
+    sign_in_submit = ttk.Button(sign_in_frame, text="Submit", command=submit)
     sign_in_submit.pack()
+
 
 def sign_up():
     sign_frame.destroy()
     sign_up_frame.pack()
-    uname_label = Label(sign_up_frame,text = "New Username")
-    password_label = Label(sign_up_frame,text = "Password")
+    uname_label = ttk.Label(sign_up_frame, text="New Username")
+    password_label = ttk.Label(sign_up_frame, text="Password")
     uname_label.pack()
-    uname_entry = Entry(sign_up_frame)
+    uname_entry = ttk.Entry(sign_up_frame)
     uname_entry.pack()
     password_label.pack()
-    password_entry = Entry(sign_up_frame, show="*")
+    password_entry = ttk.Entry(sign_up_frame, show="*")
     password_entry.pack()
-    #roles menu
-    roles_list = ['Sales Manager', 'Cashier']
+    # roles menu
+    roles_list = ["","Sales Manager", "Cashier"]
     role_inside = StringVar(sign_up_frame)
     role_inside.set("Select Role")
-    role_menu =  OptionMenu(sign_up_frame,role_inside,*roles_list)
+    role_menu = ttk.OptionMenu(sign_up_frame, role_inside, *roles_list)
     role_menu.pack()
-    
+
     def submit():
         global currentRole, currentUser
         uname = uname_entry.get()
         password = password_entry.get()
         urole = role_inside.get()
         try:
-            if uname != '' and password != '' and urole != 'Select Role':
-                cursor.execute(f"INSERT INTO user_details (uname,password,urole) VALUES ('{uname}','{password}','{urole}');")
+            if uname != "" and password != "" and urole != "Select Role" and urole != "":
+                cursor.execute(
+                    f"INSERT INTO user_details (uname,password,urole) VALUES ('{uname}','{password}','{urole}');"
+                )
                 con.commit()
-                currentUser,currentRole = uname,urole
-                sign_up_label = Label(menu_frame, text=f"Signed in as {currentUser} - {currentRole}")
+                currentUser, currentRole = uname, urole
+                sign_up_label = ttk.Label(
+                    menu_frame, text=f"Signed in as {currentUser} - {currentRole}"
+                )
                 sign_up_label.pack()
                 menu_frame.pack()
                 sign_up_frame.destroy()
         except Exception as e:
             messagebox.showerror("Error", f"Username not available \n {e}")
-    sign_up_submit = Button(sign_up_frame, text="Submit", command=submit)
-    sign_up_submit.pack()
 
+    sign_up_submit = ttk.Button(sign_up_frame, text="Submit", command=submit)
+    sign_up_submit.pack()
 
 
 def view():
@@ -120,7 +139,7 @@ def view():
             tree.column(col, width=100)
 
         for product in allproducts:
-            tree.insert('', 'end', values=product)
+            tree.insert("", "end", values=product)
 
         scrollbar = ttk.Scrollbar(data_view, orient="vertical", command=tree.yview)
         tree.configure(yscrollcommand=scrollbar.set)
@@ -132,81 +151,74 @@ def view():
         data_view.mainloop()
 
 
-
 def add():
-    
     add_win = Tk()
     add_win.title("Add New Product")
-    pname_label = Label(add_win, text= "Product Name: ")
-    price_label = Label(add_win,text = "Price: ")
-    stock_label = Label(add_win,text = "Stock:")
+    pname_label = ttk.Label(add_win, text="Product Name: ")
+    price_label = ttk.Label(add_win, text="Price: ")
+    stock_label = ttk.Label(add_win, text="Stock:")
     pname_label.pack()
-    pname_entry = Entry(add_win)
+    pname_entry = ttk.Entry(add_win)
     pname_entry.pack()
     price_label.pack()
-    price_entry = Entry(add_win)
+    price_entry = ttk.Entry(add_win)
     price_entry.pack()
     stock_label.pack()
-    stock_entry = Entry(add_win)
+    stock_entry = ttk.Entry(add_win)
     stock_entry.pack()
-    
+
     def submit():
-        cursor.execute('select pID from product order by pID desc limit 1;')
-        pID  = int(cursor.fetchall()[0][0])+1
+        cursor.execute("select pID from product order by pID desc limit 1;")
+        pID = int(cursor.fetchall()[0][0]) + 1
         pname = pname_entry.get()
         price = price_entry.get()
         stock = stock_entry.get()
-        if pname!= ''and price != ''and stock != '':
-            cursor.execute(f'insert into product values("{pID}","{pname}","{price}","{stock}"); ')
+        if pname != "" and price != "" and stock != "":
+            cursor.execute(
+                f'insert into product values("{pID}","{pname}","{price}","{stock}"); '
+            )
             con.commit()
             messagebox.showinfo("Successful", f"Product {pname} added successfully!")
             add_win.destroy()
         else:
             messagebox.showerror("Failed to Add", "Please enter valid details.")
 
-    add_product_btn = Button(add_win,text= "Add Product",command = submit)
+    add_product_btn = ttk.Button(add_win, text="Add Product", command=submit)
     add_product_btn.pack()
 
 
+currentUser = ""
+currentRole = ""
 
-
-currentUser = ''
-currentRole = ''
-
-
-
-auth_frame = Frame(root)
+auth_frame = ttk.Frame(root)
 auth_frame.pack()
-auth_label = Label(auth_frame, text="Enter MySQL Password: ")
+auth_label = ttk.Label(auth_frame, text="Enter MySQL Password: ")
 auth_label.pack()
-auth_entry = Entry(auth_frame,show = "*")
+auth_entry = ttk.Entry(auth_frame, show="*")
 auth_entry.pack()
-auth_button = Button(auth_frame, text="Authenticate", command=auth)
+auth_button = ttk.Button(auth_frame, text="Authenticate", command=auth)
 auth_button.pack()
 
-sign_frame = Frame(root)
-sign_in_btn = Button(sign_frame, text = "Sign in", command = sign_in)
+sign_frame = ttk.Frame(root)
+sign_in_btn = ttk.Button(sign_frame, text="Sign in", command=sign_in)
 sign_in_btn.pack()
-sign_up_btn = Button(sign_frame, text = "Sign up", command = sign_up)
+sign_up_btn = ttk.Button(sign_frame, text="Sign up", command=sign_up)
 sign_up_btn.pack()
 
-#sign IN
-sign_in_frame = Frame(root)
-sign_in_h1 = Label(sign_in_frame, text="Sign in")
+# sign IN
+sign_in_frame = ttk.Frame(root)
+sign_in_h1 = ttk.Label(sign_in_frame, text="Sign in")
 sign_in_h1.pack()
 
-
-#sign UP
-sign_up_frame = Frame(root)
-sign_up_h1 = Label(sign_up_frame,text = "Sign up")
+# sign UP
+sign_up_frame = ttk.Frame(root)
+sign_up_h1 = ttk.Label(sign_up_frame, text="Sign up")
 sign_up_h1.pack()
 
-
-menu_frame =Frame(root)
-view_btn = Button(menu_frame, text = "View Available Products", command = view)
+menu_frame = ttk.Frame(root)
+view_btn = ttk.Button(menu_frame, text="View Available Products", command=view)
 view_btn.pack()
-add_btn = Button(menu_frame, text = "Add Product", command = add)
+add_btn = ttk.Button(menu_frame, text="Add Product", command=add)
 add_btn.pack()
-
 
 root.mainloop()
